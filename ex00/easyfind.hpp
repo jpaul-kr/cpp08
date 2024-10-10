a3e072cd76a6de76c42e337b28b62e903fe59147
jpaul-kr <jonpk555@gmail.com>
2024-10-10 13:51:14 +0200

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,7 +2,27 @@
 # define EASYFIND_HPP
 # include <iostream>
 # include <algorithm>
+# include <list>
+# include <vector>
+# include <deque>
 
 using std::string;
+
+template <typename T>
+void	easyfind(T cont, int num)
+{
+	typename T::const_iterator	it;
+	typename T::const_iterator	ite = cont.end();
+
+	for (it = cont.begin(); it != ite ; it++)
+	{
+		if (*it == num)
+		{
+			std::cout << num << " has been found!" << std::endl;
+			return ;
+		}
+	}
+	throw std::logic_error("\tnumber not found");
+}
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,4 +2,22 @@
 
 int	main()
 {
+	int	find = 2;
+
+	std::list<int>		container;
+	//std::vector<int>	container;
+	//std::deque<int>		container;
+
+	container.push_back(1);
+	container.push_back(2);
+	container.push_back(3);
+
+	try
+	{
+		easyfind(container, find);
+	}
+	catch (std::logic_error& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
