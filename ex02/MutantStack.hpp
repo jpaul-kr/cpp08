7d5067cf4ab405a9c7cf70ec7137161188a5beea
jpaul-kr <jonpk555@gmail.com>
2024-10-18 13:11:52 +0200

    ex02 nearly done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -2,14 +2,33 @@
 # define MUTANTSTACK_HPP
 # include <iostream>
 # include <stack>
+# include <deque>
 
 using std::string;
 
-template <typename T>
-class MutantStack : std::stack<T>
+template < typename T, typename Cont = std::deque<T> >
+class MutantStack : public std::stack<T, Cont>
 {
 	public:
-		typedef std::c::iterator	iterator;
+		typedef typename Cont::iterator		iterator;
+		typedef typename Cont::const_iterator	const_iterator;
+
+		MutantStack() : std::stack<T, Cont>() {}
+		MutantStack(const MutantStack& cpy) : std::stack<T, Cont>(cpy) {}
+		~MutantStack() {}
+	
+		MutantStack&	operator=(const MutantStack& cpy)
+		{
+			if (this != &cpy)
+				std::stack<T, Cont>::operator=(cpy);
+			return *this;
+		}
+
+		iterator	begin() {return this->c.begin();}
+		iterator	end() {return this->c.end();}
+
+		const_iterator	begin() const {return this->c.begin();}
+		const_iterator	end()  const{return this->c.end();}
 
 };
 
